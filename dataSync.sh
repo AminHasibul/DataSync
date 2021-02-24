b25cf3716b44ce2986a65b8268e1d5d057e3e675
Md.Hasibul Amin <hasibul.amin009@gmail.com>
2021-02-24 22:46:01 +0600

    Update dataSync.sh

--- dataSync.sh
+++ dataSync.sh
@@ -5,7 +5,7 @@
 NOW="$(date +'%m-%d-%Y_%H-%M')"
 
 # Path to a temporary directory
-DIR=./home/custodian/mongodb_daily_dump
+DIR=./mongodb_daily_dump
 
 # Path to the target dropbox directory
 TARGET_DIR=/
