9ca6080a233915a0291aaceac7011cbf1c5eef8e
Md.Hasibul Amin <hasibul.amin009@gmail.com>
2021-02-24 19:27:10 +0600

    Update dataSync.sh
    update data sync for mongo dump data

--- dataSync.sh
+++ dataSync.sh
@@ -1,1 +1,42 @@
 
+#!/usr/bin/env bash
+
+#Get current date
+NOW="$(date +'%m-%d-%Y_%H-%M')"
+
+# Path to a temporary directory
+DIR=./mongodb_daily_dump
+
+# Path to the target dropbox directory
+TARGET_DIR=/
+
+# Name of the database
+DB_URI=$1
+DB_NAME=TlAnalytics
+
+# Name of the compressed file
+FILE="${DB_NAME}_${NOW}.tar.gz"
+
+function mongodb_dump
+{
+  # Dump the database
+  mongodump --uri $DB_URI -o $DIR
+
+  if [ $? -eq 0 ]
+  then
+    echo "✅ MongoDB dump successful"
+  else
+    echo "🔴 MongoDB dump failed"
+    exit 1
+  fi
+
+  # Compress
+  tar -zcvf $FILE $DIR
+
+  # Remove the temporary database dump directory
+  rm -fr $DIR
+}
+
+mongodb_dump
+
+fi
